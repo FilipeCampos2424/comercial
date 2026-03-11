1c50854eb8e904594df4224595c49c3d4aae12a9
FilipeCampos2424 <filipegames2343@gmail.com>
2026-03-11 16:41:31 -0300

    Formulário html consertado e começo das condições do PHP

--- atividade_autenticacao.php
+++ atividade_autenticacao.php
@@ -1,16 +1,32 @@
 
 Sistema de autenticação
 
+<!--Formulário HTML -->
 <form method="post">
 <input type="text" name="nome" id="nome" placeholder="Nome de usuário">
-<button type="submit">Enviar</button>
-</form>
-
-<form method="post">
 <input type="password" name="senha" id="senha" placeholder="Senha">
 <button type="submit">Enviar</button>
 </form>
 
-
+<!--Pegando o nome e a senha do HTML no PHP e colocando as condições pra funcionar -->
 <?php 
+if (isset($_POST{"nome"})) {
+$nome = $_POST["nome"];
+
+if (isset($_POST{"senha"})) {
+$senha = $_POST["senha"];
+
+
+if ($nome == "PROFESSOR" && $senha == "DEVISATE") {
+    echo "funcionou";
+}
+else if ($nome == "COORDENADOR" && $senha == "DEVISATE") {
+    echo "funcionou";
+}
+    else {
+    echo "funcionou nao";
+}
+
+}
+}
 ?>
