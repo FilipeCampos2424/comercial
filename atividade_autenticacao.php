1f5b4d2202d19b0889dfb2cc8a0c3c85bcd875c9
FilipeCampos2424 <filipegames2343@gmail.com>
2026-03-11 16:19:12 -0300

    Formulário html adicionado

--- atividade_autenticacao.php
+++ atividade_autenticacao.php
@@ -1,3 +1,16 @@
-<?php 
 
+Sistema de autenticação
+
+<form method="post">
+<input type="text" name="nome" id="nome" placeholder="Nome de usuário">
+<button type="submit">Enviar</button>
+</form>
+
+<form method="post">
+<input type="password" name="senha" id="senha" placeholder="Senha">
+<button type="submit">Enviar</button>
+</form>
+
+
+<?php 
 ?>
